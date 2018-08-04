6e0cd9ba18ad4cfc3e7a61ee7309a2b4dbbeee4b
Braulio Diez Botella <braulio.diez@gmail.com>
2018-08-04 20:06:43 +0200

    issues with webpack

---
+++ 00_ES6/00_start/src/hello.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+export const HelloComponent = () =>
+    <p> Hello React!</p>
---
+++ 00_ES6/00_start/webpack.config.js
@@ -0,0 +1,45 @@
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');
+let webpack = require('webpack');
+
+let basePath = __dirname;
+
+module.exports = {
+  context: path.join(basePath, "src"),
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  entry: [
+    './main.jsx',
+  ],
+  output: {
+    path: path.join(basePath, 'dist'),
+    filename: 'bundle.js',        
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist', // Content base
+    inline: true, // Enable watch and live reload
+    host: 'localhost',
+    port: 8080,
+    stats: 'errors-only'
+  },  
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: 'index.html', //Name of file in ./dist/
+      template: 'index.html', //Name of template in ./src
+      hash: true,
+    })
+  ],
+};
