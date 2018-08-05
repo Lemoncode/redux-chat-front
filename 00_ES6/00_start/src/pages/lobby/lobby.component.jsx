5ac6295d5ed1e66c964c1e675d7e568bc23c6595
Braulio Diez Botella <braulio.diez@gmail.com>
2018-08-05 11:32:11 +0200

    input plus list (no selection yet)

--- 00_ES6/00_start/src/pages/lobby/lobby.component.jsx
+++ 00_ES6/00_start/src/pages/lobby/lobby.component.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
 
 export class LobbyComponent extends React.Component {
   componentWillMount() {
     this.props.fetchRooms();
   }
 
+  onChangeTextField = (fieldId) => (e) => {
+    this.props.nicknameChange(fieldId)(e.target.value);
+  }
+
   render() {
     return (
       <div>
+        <Typography variant="display4" gutterBottom>
+          Lobby
+        </Typography>
+
+        <TextField
+          id="name"
+          label="Enter your nickname"
+          value={this.props.nickname}
+          onChange={this.onChangeTextField('nickname')}
+          margin="normal"
+        />
+
+        <List>
+          {this.props.rooms.map((room) =>
+            <ListItem key={room.id}>
+              <ListItemText
+                primary={room.name}
+              />
+            </ListItem>,
+          )}
+        </List>
+
         <ul>
           {
             this.props.rooms.map(
@@ -23,6 +54,8 @@ export class LobbyComponent extends React.Component {
 }
 
 LobbyComponent.propTypes = {
+  nickname: PropTypes.string.isRequired,
   rooms: PropTypes.array.isRequired,
-  fetchRooms: PropTypes.func.isRequired
+  fetchRooms: PropTypes.func.isRequired,
+  nicknameChange : PropTypes.func.isRequired
 };
--- 00_ES6/00_start/src/pages/lobby/lobby.container.jsx
+++ 00_ES6/00_start/src/pages/lobby/lobby.container.jsx
@@ -5,7 +5,7 @@ import { LobbyComponent } from './lobby.component';
 export class LobbyContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {rooms: []};
+    this.state = {rooms: [], nickname: ''};
 
     this.fetchRooms = this.fetchRooms.bind(this);
   }
@@ -15,9 +15,18 @@ export class LobbyContainer extends React.Component {
     this.setState({rooms});
   }
 
+  onFieldChange = (id) => (value) => {
+    this.setState({[id]: value})
+  }
+
   render() {
     return (
-      <LobbyComponent rooms={this.state.rooms} fetchRooms={this.fetchRooms}/>
+      <LobbyComponent 
+        rooms={this.state.rooms} 
+        fetchRooms={this.fetchRooms}
+        nickname={this.state.nickname}
+        nicknameChange={this.onFieldChange}
+        />
     );
   }
 }
