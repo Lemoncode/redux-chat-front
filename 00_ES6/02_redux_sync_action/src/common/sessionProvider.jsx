5c4dec81949dedeafa6d5fdca410acd49d88979b
Braulio <braulio.diez@gmail.com>
2018-08-22 09:28:14 +0200

    copied previous sample as start

---
+++ 00_ES6/02_redux_sync_action/src/common/sessionProvider.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export const SessionContext = React.createContext()
+
+export class SessionProvider extends React.Component {
+  state = {
+    sessionInfo: {room: '', nickname: ''},
+    setChatSessionInfo: (nickname, room) => {
+       this.setState({sessionInfo: {nickname, room}})
+     }
+   }
+
+   render() {
+    return <SessionContext.Provider value={this.state}>
+            {this.props.children}
+           </SessionContext.Provider>     
+   }
+ }
+
+ export const withSessionContext = (Component) => (props) => (
+  <SessionContext.Consumer>
+     {
+        ({ sessionInfo, setChatSessionInfo }) => (
+           <Component
+                 {...props}
+                 sessionInfo={sessionInfo}
+                 setChatSessionInfo={setChatSessionInfo}
+           />
+        )
+     }
+ </SessionContext.Consumer>
+);
