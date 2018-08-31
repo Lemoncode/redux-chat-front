93c6065988634ce272e6ffd9a2431336142b4add
Javier Calzado <fjcalzadorodriguez@gmail.com>
2018-08-31 10:00:20 +0200

    Initial push. 05 copied into 06.

---
+++ 00_ES6/06_jest_setup/src/common/card-layout.component.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { withStyles } from "@material-ui/core/styles";
+import styles from './card-layout.styles';
+
+
+const CardLayoutInner = (props) => (
+  <div className={props.classes.container}>
+    {props.children}
+  </div>
+);
+
+export const CardLayout = withStyles(styles)(CardLayoutInner);
